3ac7c13c6849ebfe618855dfba54ed771458b532
Matthew York <myorkprogramming@gmail.com>
2023-08-15 22:53:50 -0700

    Matrix Search

--- index.js
+++ index.js
@@ -1,53 +1,4 @@
-//1.
-//Median of Sorted Arrays
-
-/* Given two arrays that are sorted but not necessarily the same length, 
-find the median value. Given ([1,5,9], [1,2,3,4,5,6]), 
-return 4. If the number of values is even, 
-return the average of the two middle values. 
-Given ([1,5,9], [1,2,3,4,5]), return 3.5.
-
-Second: accept three arrays instead of two. */
-
-function medianOfSortedArrays(arr1, arr2){
-    var arr3 = [];
-    var median = 0;
-
-    for(var i = 0; i < arr1.length; i++){
-        arr3.push(arr1[i]);
-    }
-
-    for(var i = 0; i < arr2.length; i++){
-        arr3.push(arr2[i]);
-    }
-
-    arr3.sort(function(a, b){return a-b});
-
-    if(arr3.length % 2 == 0){
-        median = (arr3[arr3.length/2] + arr3[(arr3.length/2)-1])/2;
-    } else {
-        median = arr3[Math.floor(arr3.length/2)];
-    }
-
-    return median;
-}
-
-//Example:
-console.log(medianOfSortedArrays([1,5,9], [1,2,3,4,5,6]));
-
-//We create an empty array.
-//We create a median variable.
-//We loop through the first array and push the values into the empty array.
-//We loop through the second array and push the values into the empty array.
-//We sort the array.
-//We check if the array length is even.
-//If it is, we set the median to the average of the two middle values.
-//If it is not, we set the median to the middle value.
-//We return the median.
-
-//#########################################################################################################
-
-//2.
+//1
 //Last Digit of A to the B
 
 /* Modern computers can handle very large numbers, but they do have their limits. 
@@ -92,3 +43,19 @@ console.log(lastDigitAtoB(237,124));
 //We return the last digit.
 
 //#########################################################################################################
+
+//2
+//Matrix Search
+
+/* Mike digs image recognition and wants to create a JavaScript Imaging Library, 
+just like PIL for Python. 
+He is given 2 different two-dimensional arrays, 
+containing integers between 0 and 65535. 
+Each two-dimensional array represents a gray-scale image, 
+where each integer value is a pixel. T
+he second image might be found somewhere within the larger one. 
+Return whether it is.
+
+
+Second: Return location of first match found ([-1,-1] if no match). 
+In example above, return [2,1]. */
