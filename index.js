7da2d0e39d09f6bafd820083a556dbd18060459b
Matthew York <myorkprogramming@gmail.com>
2023-08-16 00:19:38 -0700

    Max Subarray Sums

--- index.js
+++ index.js
@@ -117,3 +117,19 @@ console.log(matrixSearch(arr1,arr2));
 //If found is false, we return [-1,-1].
 
 //#########################################################################################################
+
+//3
+//Max of Subarray Sums
+
+/* How efficient can you be in this following final challenge? 
+The input may hold many million values.
+
+Given a numerical array that is potentially very long, 
+return the maximum sum of values from a subarray. 
+Any consecutive sequence of indices in the array is considered a subarray. 
+Create a function that returns the highest sum possible from these subarrays. 
+Given [1,2,-4,3,-2,3,-1], you should return 4 (for subarray [3,-2,3]), 
+and given [-1,-2,-4,-3,-2,-3], return 0 (for []). 
+This problem has many possible implementations. 
+Which do you prefer & why? 
+*/
